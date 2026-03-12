9f5314ac2c3d47b748381a126a6bbcda49d8d4ca
yanxc76 <yanandreotti2@gmail.com>
2026-03-12 10:58:38 -0300

    Update SomaVetorComCorrida.c

--- SomaVetorComCorrida.c
+++ SomaVetorComCorrida.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
+#define NUM_THREADS 8
 #define VECTOR_SIZE 100000000
 
 // Variáveis globais
@@ -19,18 +19,14 @@ typedef struct {
 // Função executada por cada thread
 void* sum_worker(void* args) {
     ThreadArgs* my_args = (ThreadArgs*)args;
-    long long local_sum = 0;
+    
 
     // 1. Cada thread calcula a soma da sua fatia
     for (int i = my_args->start; i < my_args->end; i++) {
-        local_sum += vector[i];
+        global_sum += vector[i];
     }
     
-    printf("Thread %d calculou a soma parcial: %lld\n", my_args->thread_id, local_sum);
-
-    // 2. Condição de Corrida! Múltiplas threads tentam atualizar
-    // a variável global ao mesmo tempo.
-    global_sum += local_sum;
+    
 
     free(my_args); // Libera a memória dos argumentos
     return NULL;
@@ -78,3 +74,4 @@ int main() {
     free(vector);
     return 0;
 }
+
